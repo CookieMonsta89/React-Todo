7d6576c22bf7d1c8625dd0ab1e875d0c561be0fd
Joe Stanfield <jlstan06@hotmail.com>
2018-06-13 17:37:04 -0400

    Joe Stanfield initial commit

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Todo extends React.Component {
+    constructor (props) {
+        super();
+        this.state = {
+            task: props.task,
+            isCompleted: props.isCompleted
+        }
+    }
+
+
+
+    render() {
+        return(
+            <li>{this.state.task}</li>
+        );
+    }
+}
+
+
+export default Todo;
