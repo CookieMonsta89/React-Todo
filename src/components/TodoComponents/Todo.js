f69e3a5c31d6c018542f77e9572fe3284bb98291
Joe Stanfield <jlstan06@hotmail.com>
2018-06-14 17:25:19 -0400

    styling added

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -7,7 +7,7 @@ import './Todo.css'
 const Todo = (props) => {
     
     return(
-        <li> {props.todo} </li>////putting array into a list 
+        <li onClick={props.onClick} className="list">{props.todo} </li>////putting array into a list 
     )
 }
 
